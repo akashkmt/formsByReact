76b8db0cc24138e88fc166e7f0e694af6d55fef4
akashkmt <akashkmt963@gmail.com>
2022-06-13 12:33:43 +0530

    update pagination

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,63 +2,75 @@ import React from "react";
 import EmployeeForm from "./form";
 import MakeTable from "./table";
 
-export default function Main (){
+export default function Main() {
+  const [allEmployeeData, setAllEmployeeData] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(false);
+  const [page, setPage] = React.useState(1);
 
-    const [allEmployeeData, setAllEmployeeData] = React.useState([]);
-    const [loading, setLoading] = React.useState(false);
-    const [error, setError] = React.useState(false);
-    const [page, setPage] = React.useState(1);
+  const [lastPage, setLastPage] = React.useState();
 
-    const [lastPage, setLastPage] = React.useState()
-
-    const addEmployeeToDB = async (employeeData) => {
-        try {
-            await fetch(`http://localhost:3001/employeeDetails`,{
-                method : "POST",
-                body : JSON.stringify(employeeData),
-                headers : {"Content-Type" : "application/json"}
-            });
-            getEmployeeData();
-        } catch (error) {
-            console.log(error);
-            setError(true);
-        }
+  const addEmployeeToDB = async (employeeData) => {
+    try {
+      await fetch(`http://localhost:3001/employeeDetails`, {
+        method: "POST",
+        body: JSON.stringify(employeeData),
+        headers: { "Content-Type": "application/json" },
+      });
+      getEmployeeData();
+    } catch (error) {
+      console.log(error);
+      setError(true);
     }
+  };
 
-    const getEmployeeData = async () => {
-        try {
-            setLoading(true);
-            let response =await fetch(`http://localhost:3001/employeeDetails?_page=${page}&_limit=5`);
-            let result = await response.json();
-            setAllEmployeeData(result);
+  const getEmployeeData = async () => {
+    try {
+      setLoading(true);
+      let response = await fetch(
+        `http://localhost:3001/employeeDetails?_page=${page}&_limit=5`
+      );
+      let result = await response.json();
+      setAllEmployeeData(result);
 
-            for(var pair of response.headers.entries()){
-                if(pair[0] === 'x-total-count'){
-                    setLastPage(Math.ceil(pair[1]/5));
-                }
-            }
-        } catch (error) {
-            console.log(error)
-            setError(true);
-        }
-        setLoading(false);
+      // for(var pair of response.headers.entries()){
+      //     if(pair[0] === 'x-total-count'){
+      //         setLastPage(Math.ceil(pair[1]/5));
+      //     }
+      // }
+      let last = response.headers.get("X-Total-Count");
+      setLastPage(Math.ceil(last / 5));
+    } catch (error) {
+      console.log(error);
+      setError(true);
     }
+    setLoading(false);
+  };
 
-    const handlePage = (value) => {
-        setPage(page+value);
-        // console.log(page);
-    }
+  const handlePage = (value) => {
+    setPage(page + value);
+    // console.log(page);
+  };
 
-    React.useEffect(()=>{
-        getEmployeeData();
-    },[page]);
+  React.useEffect(() => {
+    getEmployeeData();
+  }, [page]);
 
-    return (
-        <>
-        <EmployeeForm addEmployeeToDB={addEmployeeToDB} />
-        {
-            loading ? (<h1>Loading</h1>) : error ? (<h1>Something went wrong</h1>) : (<MakeTable allEmployeeData={allEmployeeData} handlePage={handlePage} page={page} lastPage={lastPage}/>)
-        }
-        </>
-    )
+  return (
+    <>
+      <EmployeeForm addEmployeeToDB={addEmployeeToDB} />
+      {loading ? (
+        <h1>Loading</h1>
+      ) : error ? (
+        <h1>Something went wrong</h1>
+      ) : (
+        <MakeTable
+          allEmployeeData={allEmployeeData}
+          handlePage={handlePage}
+          page={page}
+          lastPage={lastPage}
+        />
+      )}
+    </>
+  );
 }
--- src/components/table.jsx
+++ src/components/table.jsx
@@ -1,42 +1,49 @@
 import React from "react";
 
-export default function MakeTable ({allEmployeeData, handlePage , page, lastPage}) {
-    // console.log(allEmployeeData);
-    return (
-        <div id="tableDiv">
-        <h2 style={{marginTop:"30px"}}>All Employee Details</h2>
-        <table id="employeeTable">
-            <thead>
-                <tr>
-                    <th className="thAndTd">Id</th>
-                    <th className="thAndTd">Name</th>
-                    <th className="thAndTd">Age</th>
-                    <th className="thAndTd">Department</th>
-                    <th className="thAndTd">Married</th>
-                    <th className="thAndTd">Salary</th>
-                    <th className="thAndTd">Address</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    allEmployeeData.map(item => {
-                        return (
-                            <tr key={item.id}>
-                                <td className="thAndTd">{item.id}</td>
-                                <td className="thAndTd">{item.name}</td>
-                                <td className="thAndTd">{item.age}</td>
-                                <td className="thAndTd">{item.department}</td>
-                                <td className="thAndTd">{item.isMarried ? "Yes" : "No"}</td>
-                                <td className="thAndTd">{item.salary}</td>
-                                <td className="thAndTd">{item.address}</td>
-                            </tr>
-                        )
-                    })
-                }
-            </tbody>
-        </table>
-        <button onClick={()=>handlePage(-1)} disabled={page === 1}>PREV</button>
-        <button onClick={()=>handlePage(1)} disabled={page === lastPage}>NEXT</button>
-        </div>
-    )
+export default function MakeTable({
+  allEmployeeData,
+  handlePage,
+  page,
+  lastPage,
+}) {
+  // console.log(allEmployeeData);
+  return (
+    <div id="tableDiv">
+      <h2 style={{ marginTop: "30px" }}>All Employee Details</h2>
+      <table id="employeeTable">
+        <thead>
+          <tr>
+            <th className="thAndTd">Id</th>
+            <th className="thAndTd">Name</th>
+            <th className="thAndTd">Age</th>
+            <th className="thAndTd">Department</th>
+            <th className="thAndTd">Married</th>
+            <th className="thAndTd">Salary</th>
+            <th className="thAndTd">Address</th>
+          </tr>
+        </thead>
+        <tbody>
+          {allEmployeeData.map((item) => {
+            return (
+              <tr key={item.id}>
+                <td className="thAndTd">{item.id}</td>
+                <td className="thAndTd">{item.name}</td>
+                <td className="thAndTd">{item.age}</td>
+                <td className="thAndTd">{item.department}</td>
+                <td className="thAndTd">{item.isMarried ? "Yes" : "No"}</td>
+                <td className="thAndTd">{item.salary}</td>
+                <td className="thAndTd">{item.address}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <button onClick={() => handlePage(-1)} disabled={page === 1}>
+        PREV
+      </button>
+      <button onClick={() => handlePage(1)} disabled={page === lastPage}>
+        NEXT
+      </button>
+    </div>
+  );
 }
